61bcfb6e87b4318953e75602d979e838331ec200
ana <anacsilveirasouza@gmail.com>
2023-11-01 12:05:05 -0300

    implementando testes automatizados no login

--- cleanUpSite/tests.py
+++ cleanUpSite/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class LoginSiteViewTest(TestCase):
+    def setUp(self):
+        # Criar um usuário de teste com credenciais válidas
+        self.user = User.objects.create_user(username='testuser', password='testpassword1')
+
+    def test_login_with_valid_credentials(self):
+        # Faça uma solicitação POST para a view com as credenciais válidas configuradas em setUp
+        response = self.client.post(reverse('home'), {
+            'username': 'testuser',
+            'senha': 'testpassword',
+        })
+
+        # Verifique se a resposta redireciona para a página minhaConta.html
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_login_with_invalid_credentials(self):
+        # Faça uma solicitação POST para a view com credenciais inválidas
+        response = self.client.post(reverse('home'), {
+            'username': 'invaliduser',
+            'senha': 'invalidpassword',
+        })
+
+        # Verifique se a resposta permanece na página home.html e contém o erro
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+      #  self.assertContains(response, 'Dados errados')
+
+
+
